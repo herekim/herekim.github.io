7b6c6217b943535cb0f031f94364fa636324e4df
herekim <khere220@gmail.com>
2022-04-28 17:57:04 +0900

    Updates

---
+++ component---src-pages-404-js-0ebf3e25b4829360103e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalwayshere=self.webpackChunkalwayshere||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),l=a(8678),s=a(262);t.default=function(e){var t=e.data,a=e.location,u=t.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:u},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0ebf3e25b4829360103e.js.map
